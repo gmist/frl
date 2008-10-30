06a92a34525cc768064e8aca5e56cd1fb1262fae
gmist <serg.baburin@gmail.com>
2008-10-30 19:18:32 +0000

    Remove outdated frl::unit_test (all replaced with boost::unit_test);
    git-svn-id: http://frl.googlecode.com/svn/trunk@139 daf5c975-553e-0410-8cda-3135a9dfd8f3

--- include/frl_unit_test.h
+++
@@ -1,9 +0,0 @@
-#ifndef frl_unit_test_h_header_
-#define frl_unit_test_h_header_
-
-#include "frl_platform.h"
-#include "unit_test\frl_console_out_policy.h"
-#include "unit_test\frl_unit_test_statistics.h"
-#include "unit_test\frl_unit_test.h"
-
-#endif // frl_unit_test_h_header_
