fe5fcf90e60e4d7b571c6cd2f5681229c32ed9b6
gmist <serg.baburin@gmail.com>
2008-10-30 09:23:41 +0000

    Rename test project test_io_win32_registry to test_os_win32_registry;
    git-svn-id: http://frl.googlecode.com/svn/trunk@133 daf5c975-553e-0410-8cda-3135a9dfd8f3

--- build_tools/mxx_ru/template/build_debug_tmpl.bat
+++ build_tools/mxx_ru/template/build_debug_tmpl.bat
@@ -2,7 +2,7 @@ cd debug
 ruby frl.dependency.vendors.opc_foundation.debug.rb
 ruby frl.lib.debug.rb
 ruby frl.test.fs_fn.debug.rb
-ruby frl.test.io_win32_registry.debug.rb
+ruby frl.test.os_win32_registry.debug.rb
 ruby frl.test.lexical_cast.debug.rb
 ruby frl.test.logging.debug.rb
 ruby frl.test.opc_address_space.debug.rb
--- build_tools/mxx_ru/template/build_release_tmpl.bat
+++ build_tools/mxx_ru/template/build_release_tmpl.bat
@@ -2,7 +2,7 @@ cd release
 ruby frl.dependency.vendors.opc_foundation.release.rb
 ruby frl.lib.release.rb
 ruby frl.test.fs_fn.release.rb
-ruby frl.test.io_win32_registry.release.rb
+ruby frl.test.os_win32_registry.release.rb
 ruby frl.test.lexical_cast.release.rb
 ruby frl.test.logging.release.rb
 ruby frl.test.opc_address_space.release.rb
